30c6393fd9dbe65b32d64f2e1e3b99312ac9fe24
chaungo <chaungo@tamu.edu>
2017-03-21 23:29:57 -0500

    updated Player

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,4 @@
-import java.util.Scanner;
-
 public class Player {
-	static final Scanner scanner = new Scanner(System.in);
 	private String username;
 	int score;
 	boolean move = false;
@@ -10,8 +7,8 @@ public class Player {
 		return username;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setUsername(String userame) {
+		this.username = userame;
 	}
 
 	public int getScore() {
@@ -29,24 +26,26 @@ public class Player {
 	public void setMove(boolean move) {
 		this.move = move;
 	}
-	public void makeMove(Board board, int seed, int from_house, int to_house) {
+
+	public boolean makeMove(Board board, int seed, int from_house, int to_house) {
 		// Move seed on the board.
 		// Ask board.game_manager to check whether this move is available or not
-		// bool check = board.game_manager.checkvalid()
+		// boolean check = board.game_manager.checkvalid()
 		// if it is validate(true) => update the board and score
 		// else give a message to player that this move is not available
-		
-	}
-
-	// Override toString to display the correct amount information
-	public String toString() {
-		return ("\nName: " + username + "\nScore: " + score);
+		boolean check = board.game_manager.is_valid_move(from_house);
+		if (check) {
+			board.sowSeeds(from_house);
+			return true;
+		} else {
+			System.out.println(" invalid move");
+			return false;
+		}
 	}
 
 	// Constructor
-	public Player(String player_name, int player_score) {
+	public Player(String player_name) {
 		setUsername(player_name);
-		setScore(player_score);
 		// setMove(player_move);
 	}
 }
