995cacc50127b0d816503f5b63ab52772c92af84
chaungo <chaungo@tamu.edu>
2017-03-30 00:34:59 -0500

    update num of house and take input from user

--- src/KalahGUI.java
+++ src/KalahGUI.java
@@ -42,6 +42,7 @@ public class KalahGUI {
 	private String seeds_per;
 	private static final String default_house = "6";
 	private static final String default_seeds = "4";
+	private JTextField textFieldHouse;
 
 	/**
 	 * Launch the application.
@@ -71,37 +72,36 @@ public class KalahGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame("Kalah Game");
-		frame.setBounds(100, 100, 460, 306);
+		frame.setBounds(100, 100, 508, 389);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		JPanel panel = new JPanel();
-		panel.setBounds(45, 11, 311, 31);
+		panel.setBounds(91, 11, 311, 31);
 		panel.setBorder(new LineBorder(new Color(0, 0, 0), 0));
 		panel.setBackground(Color.WHITE);
 		panel.setPreferredSize(new Dimension(25, 50));
 		frame.getContentPane().add(panel);
-		
+
 		JLabel welcome = new JLabel("Welcome to Kalah");
 		welcome.setForeground(Color.BLUE);
 		welcome.setFont(new Font("Tamaho", Font.PLAIN, 23));
 		panel.add(welcome);
 		panel.setPreferredSize(new Dimension(300, 50));
-		
+
 		JLabel username = new JLabel("Enter username");
 		username.setBounds(10, 61, 116, 22);
 		frame.getContentPane().add(username);
-		
+
 		textFieldUsername = new JTextField();
-		textFieldUsername.setBounds(112, 61, 145, 20);
+		textFieldUsername.setBounds(112, 63, 145, 20);
 		frame.getContentPane().add(textFieldUsername);
 		textFieldUsername.setColumns(10);
-		
-		//save username for player constructor
-		
-		
-		//save player number for player constructor
-		
+
+		// save username for player constructor
+
+		// save player number for player constructor
+
 		/*
 		 * JButton btnSaveUsername = new JButton("Save");
 		 * btnSaveUsername.addActionListener(new ActionListener() { public void
@@ -109,26 +109,28 @@ public class KalahGUI {
 		 * }); btnSaveUsername.setBounds(267, 61, 89, 23);
 		 * frame.getContentPane().add(btnSaveUsername);
 		 */
-		
+
 		JLabel seedsNum = new JLabel("Number of seeds");
-		seedsNum.setBounds(10, 94, 116, 36);
+		seedsNum.setBounds(10, 103, 116, 36);
 		frame.getContentPane().add(seedsNum);
-		
+
 		textFieldSeeds = new JTextField();
-		textFieldSeeds.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-		
-				seeds_per = textFieldSeeds.getText();
-				//seedsNum.setText(input);
-				//JOptionPane.showConfirmDialog(null, seedsNum);
-			}
-		});
-		textFieldSeeds.setBounds(112, 102, 108, 20);
+		textFieldSeeds.setBounds(112, 111, 108, 20);
 		frame.getContentPane().add(textFieldSeeds);
 		textFieldSeeds.setColumns(10);
 
+		JLabel houseNum = new JLabel("Number of houses");
+		houseNum.setBounds(10, 153, 108, 14);
+		frame.getContentPane().add(houseNum);
+		
+		textFieldHouse = new JTextField();
+		textFieldHouse.setBounds(112, 150, 108, 20);
+		frame.getContentPane().add(textFieldHouse);
+		textFieldHouse.setColumns(10);
+
+		
 		JLabel difficulty = new JLabel("Difficulty");
-		difficulty.setBounds(10, 141, 75, 22);
+		difficulty.setBounds(10, 194, 75, 22);
 		frame.getContentPane().add(difficulty);
 		// difficulty button
 		JCheckBox checkBox1 = new JCheckBox("Easy");
@@ -138,55 +140,65 @@ public class KalahGUI {
 		group.add(checkBox1);
 		group.add(checkBox2);
 		group.add(checkBox3);
-		checkBox1.setBounds(112, 141, 75, 22);
+		checkBox1.setBounds(112, 194, 75, 22);
 		frame.getContentPane().add(checkBox1);
-		checkBox2.setBounds(112, 168, 90, 22);
+		checkBox2.setBounds(112, 230, 90, 22);
 		frame.getContentPane().add(checkBox2);
-		checkBox3.setBounds(112, 187, 129, 36);
+		checkBox3.setBounds(112, 254, 129, 36);
 		frame.getContentPane().add(checkBox3);
 
 		// save seeds button
-		JButton btnSaveSeeds = new JButton("Save");
-		btnSaveSeeds.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				try {
-					int temp = Integer.parseInt(seeds_per);
-					if (temp < 10 && temp > 0) {
-						
-					}
-					else {
-						JOptionPane.showMessageDialog(null, "Please enter a Number from 1 to 10");
-					}
-				} catch (Exception e1) {
-					JOptionPane.showMessageDialog(null, "Please enter a Number from 1 to 10");
-				}
-				btnSaveSeeds.setText("Saved");
-
-			}
-		});
-		btnSaveSeeds.setBounds(267, 101, 89, 23);
-		frame.getContentPane().add(btnSaveSeeds);
-
+		/*
+		 * JButton btnSaveSeeds = new JButton("Save");
+		 * btnSaveSeeds.addActionListener(new ActionListener() { public void
+		 * actionPerformed(ActionEvent e) { try { if (seeds_per == null) {
+		 * seeds_per = textFieldSeeds.getText(); } int temp =
+		 * Integer.parseInt(seeds_per); if (temp <= 10 && temp > 0) {
+		 * 
+		 * } else { JOptionPane.showMessageDialog(null,
+		 * "Please enter a Number from 1 to 10"); } } catch (Exception e1) {
+		 * JOptionPane.showMessageDialog(null,
+		 * "Please enter a Number from 1 to 10"); }
+		 * btnSaveSeeds.setText("Saved");
+		 * 
+		 * } }); btnSaveSeeds.setBounds(267, 101, 89, 23);
+		 * frame.getContentPane().add(btnSaveSeeds);
+		 */
 		// next button
 		JButton btnNext = new JButton("Next >");
 		btnNext.addActionListener(new ActionListener() {
-			//go to the next frame
+			// go to the next frame
 			public void actionPerformed(ActionEvent arg0) {
-				
-				frame.dispose();
-				Game game = new Game(houses_per, seeds_per);
-				game.setVisible(true);
+				String username = textFieldUsername.getText();
+				seeds_per = textFieldSeeds.getText();
+				houses_per = textFieldHouse.getText();
+				if (username.equals("") || seeds_per.equals("") || group.getSelection() == null || houses_per.equals("")) {
+					JOptionPane.showMessageDialog(null, "Please fill in all the blank and select difficulty ");
+				} else {
+					int seedsTemp = Integer.parseInt(seeds_per);
+					int houseTemp = Integer.parseInt(houses_per);
+					if (seedsTemp > 10 || seedsTemp < 1 || houseTemp > 9 || houseTemp < 4) {
+						JOptionPane.showMessageDialog(null, "Please enter a Number from 1 to 10");
+					} else {
+
+						frame.dispose();
+						Game game = new Game(houses_per, seeds_per);
+						game.setVisible(true);
+					}
+				}
 			}
 		});
-		btnNext.setBounds(305, 233, 89, 23);
+		btnNext.setBounds(351, 300, 89, 23);
 		frame.getContentPane().add(btnNext);
-
-	/*	// image
-		JLabel lblImage = new JLabel(" ");
-		Image img = new ImageIcon(this.getClass().getResource("welcome-icon.png")).getImage();
-		lblImage.setIcon(new ImageIcon(img));
-		lblImage.setBounds(275, 53, 166, 256);
-		frame.getContentPane().add(lblImage);*/
+		
+		
+		
+		
+		/*
+		 * // image JLabel lblImage = new JLabel(" "); Image img = new
+		 * ImageIcon(this.getClass().getResource("welcome-icon.png")).getImage()
+		 * ; lblImage.setIcon(new ImageIcon(img)); lblImage.setBounds(275, 53,
+		 * 166, 256); frame.getContentPane().add(lblImage);
+		 */
 	}
-	
 }
