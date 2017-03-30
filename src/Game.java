67f6f4a83c0f7e88227edd7811c58247b7aec0d8
chaungo <chaungo@tamu.edu>
2017-03-30 00:35:27 -0500

    update Game.java

--- src/Game.java
+++ src/Game.java
@@ -20,9 +20,13 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Button;
-
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Vector;
+import java.awt.GridLayout;
+import javax.swing.border.LineBorder;
+import java.awt.event.ActionListener;
 //import java.awt.event.InputMethodListener;
 //import java.awt.event.InputMethodEvent;
 
@@ -31,7 +35,9 @@ public class Game extends JFrame {
 	private JPanel contentPane;
 	private JPanel content;
 	private JFrame frame;
-	private JButton[][] buttons;
+	// private JButton[][] buttons;
+	private Vector<JButton> buttons_south = new Vector<JButton>();
+	private Vector<JButton> buttons_north = new Vector<JButton>();
 	private Queue<String> moves;
 	private boolean moveleftToRight;
 	static Board board;
@@ -40,12 +46,10 @@ public class Game extends JFrame {
 	 * sLaunch the application.
 	 */
 
-	public void makeMove(int location) {
-		System.out.print("Updating Game\n");
-		board.sowSeeds(location);
-	}
-
 	public static void main(String[] args) {
+		// Game a = new Game("4", "5");
+		// a.setVisible(true);
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -66,91 +70,114 @@ public class Game extends JFrame {
 	 * Create the frame.
 	 */
 	public Game(String houses_per, String seeds_per) {
+		int house = Integer.parseInt(houses_per);
+		int seeds = Integer.parseInt(seeds_per);
+		board = new Board(house, seeds);
 		setTitle("Kalah Game");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 631, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
 
-		//moves = new Queue<String>;
-		
-		 //set variable number of buttons
-		 //make buttons and store in matrix
-		   	for (int i = 0; i < 2; i++) {
-		   		for (int j = 0; j < Integer.parseInt(houses_per)*2; i++) {
-		   			buttons[i][j] = new JButton("j");
-		   		}
-		 	}
-		
-		//with one action listener to rule them all
-		ActionListener a = new ActionListener() {
+		JPanel panelWest = new JPanel();
+		contentPane.add(panelWest, BorderLayout.WEST);
+		JPanel storeLabel1 = new JPanel();
+		storeLabel1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		storeLabel1.setForeground(Color.BLUE);
+		storeLabel1.setBackground(Color.WHITE);
+		JLabel store1 = new JLabel("Store 1");
+		storeLabel1.add(store1);
+		panelWest.add(storeLabel1);
 
-			   public void actionPerformed(ActionEvent evt) {
-			       if (evt.getSource() == buttons[0][0]) {
+		JPanel panelEast = new JPanel();
+		contentPane.add(panelEast, BorderLayout.EAST);
+		JPanel storeLabel2 = new JPanel();
+		storeLabel2.setBorder(new LineBorder(new Color(0, 0, 0)));
+		storeLabel2.setForeground(Color.BLUE);
+		storeLabel2.setBackground(Color.WHITE);
+		JLabel store2 = new JLabel("Store 2");
+		storeLabel2.add(store2);
+		panelEast.add(storeLabel2);
 
-			       }
-			       // etc
-			       // common handling
-			   }
-			};
+		JPanel panelNorth = new JPanel();
+		contentPane.add(panelNorth, BorderLayout.NORTH);
+		panelNorth.setPreferredSize(new Dimension(50, 50));
 
-			for (int i = 0; i < 2; ++i){
-				  for (int j = 0; j < Integer.parseInt(houses_per); ++j) {
-					    buttons[i][j].addActionListener(a);
-				  }
-			}
-		
+		JPanel panelSouth = new JPanel();
+		contentPane.add(panelSouth, BorderLayout.SOUTH);
+		panelSouth.setPreferredSize(new Dimension(50, 50));
+
+		int nhouse_per = Integer.parseInt(houses_per);
+		panelSouth.setLayout(new GridLayout(1, nhouse_per));
+		panelNorth.setLayout(new GridLayout(1, nhouse_per));
+
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new GridLayout(1, 2));
+		JPanel panel_button = new JPanel();
+		JPanel panel_label = new JPanel();
+		JButton reset = new JButton("Reset");
+		reset.setSize(10, 10);
+		panel_button.add(reset);
+		JLabel time = new JLabel("Time");
+		panel_button.add(time);
+		panel.add(panel_button);
+		panel.add(panel_label);
+
+		// panel.add(new JButton("A"));
+		// panel.setSize(10,10);
+		// panel.add(new JLabel("Bdsfsdf"));
+		// panel.setSize(10,10);
+		ButtonsListener button_listener = new ButtonsListener();
+		for (int i = 0; i < nhouse_per; i++) {
+			JButton button_south = new JButton(seeds_per);
+			JButton button_north = new JButton(seeds_per);
+			button_south.addActionListener(button_listener);
+			button_north.addActionListener(button_listener);
+
+			buttons_south.add(button_south);
+			buttons_north.add(button_north);
+
+			panelSouth.add(button_south);
+			panelNorth.add(button_north);
+
+		}
+		// contentPane.setLayout(new GridLayout(rows, cols));
+		// contentPane.add(new JButton("A"));
+		// contentPane.add(new JButton("B"));
+
+		// moves = new Queue<String>;
+
+		// set variable number of buttons
+		// make buttons and store in matrix
 		/*
-		 *add image to each of the buttons, if necessary (perhaps color code them for valid/invalid?)
+		 * for (int i = 0; i < 2; i++) { for (int j = 0; j <
+		 * Integer.parseInt(houses_per)*2; i++) { buttons[i][j] = new
+		 * JButton("j"); } }
+		 * 
+		 * //with one action listener to rule them all ActionListener a = new
+		 * ActionListener() {
+		 * 
+		 * public void actionPerformed(ActionEvent evt) { if (evt.getSource() ==
+		 * buttons[0][0]) {
+		 * 
+		 * } // etc // common handling } };
+		 * 
+		 * for (int i = 0; i < 2; ++i){ for (int j = 0; j <
+		 * Integer.parseInt(houses_per); ++j) {
+		 * buttons[i][j].addActionListener(a); } }
+		 * 
+		 * /* add image to each of the buttons, if necessary (perhaps color code
+		 * them for valid/invalid?)
 		 *
-   for(int i=0;i<30;i++){
-       buttons[i] = new JButton("label"+ i);
-       buttons[i].setBackground(Color.white);
-       if (i < 10) {
-          if (i%2 == 0) {
-            buttons[i].setIcon(piece2);
-          } else {
-            buttons[i].setIcon(piece1);
-          }
-       }
-       panel.add(buttons[i]);
-   }
+		 * for(int i=0;i<30;i++){ buttons[i] = new JButton("label"+ i);
+		 * buttons[i].setBackground(Color.white); if (i < 10) { if (i%2 == 0) {
+		 * buttons[i].setIcon(piece2); } else { buttons[i].setIcon(piece1); } }
+		 * panel.add(buttons[i]); }
 		 */
 		
-		
-		ButtonsListener listener = new ButtonsListener();
-		
-		JButton button1 = new JButton("1");
-		button1.addActionListener(listener);
-
-		JButton button2 = new JButton("2");
-		button2.addActionListener(listener);
-
-		JButton button3 = new JButton("3");
-		button3.addActionListener(listener);
-
-		JButton button4 = new JButton("4");
-		button4.addActionListener(listener);
-
-		JButton button5 = new JButton("5");
-		button5.addActionListener(listener);
-
-		JButton button_1 = new JButton("1");
-		button_1.addActionListener(listener);
-
-		JButton button_2 = new JButton("2");
-		button_2.addActionListener(listener);
-
-		JButton button_3 = new JButton("3");
-		button_3.addActionListener(listener);
-
-		JButton button_4 = new JButton("4");
-		button_4.addActionListener(listener);
-
-		JButton button_5 = new JButton("5");
-		button_5.addActionListener(listener);
-
 		JButton btnReset = new JButton("Reset");
 		btnReset.setBackground(Color.WHITE);
 		btnReset.setForeground(Color.BLUE);
@@ -169,81 +196,13 @@ public class Game extends JFrame {
 		 * 256); frame.getContentPane().add(lblImage);
 		 */
 		JLabel lblHouse2 = new JLabel("House2");
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
-				.createSequentialGroup()
-				.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(lblTime, Alignment.LEADING, GroupLayout.DEFAULT_SIZE,
-								GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(btnReset, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE,
-								Short.MAX_VALUE))
-				.addGap(14)
-				.addGroup(
-						gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-										.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 81,
-												GroupLayout.PREFERRED_SIZE)
-										.addGap(18)
-										.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 81,
-												GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.UNRELATED)
-										.addComponent(button_3, GroupLayout.PREFERRED_SIZE, 81,
-												GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.UNRELATED)
-										.addComponent(button_4, GroupLayout.PREFERRED_SIZE, 81,
-												GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.RELATED).addComponent(button_5,
-												GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE))
-								.addGroup(gl_contentPane.createSequentialGroup()
-										.addComponent(button1, GroupLayout.PREFERRED_SIZE, 81,
-												GroupLayout.PREFERRED_SIZE)
-										.addGap(18)
-										.addComponent(button2, GroupLayout.PREFERRED_SIZE, 81,
-												GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.UNRELATED)
-										.addComponent(button3, GroupLayout.PREFERRED_SIZE, 81,
-												GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.UNRELATED)
-										.addComponent(button4, GroupLayout.PREFERRED_SIZE, 81,
-												GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.RELATED).addComponent(button5,
-												GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)))
-				.addContainerGap(73, Short.MAX_VALUE))
-				.addGroup(gl_contentPane.createSequentialGroup().addContainerGap().addComponent(lblHouse1)
-						.addPreferredGap(ComponentPlacement.RELATED, 485, Short.MAX_VALUE).addComponent(lblHouse2)
-						.addGap(28)));
-		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup().addComponent(lblTime).addGap(17)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(button1, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-								.addComponent(button2, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-								.addComponent(button3, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-								.addComponent(button4, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-								.addComponent(button5, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE))
-						.addGap(28)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(lblHouse1)
-								.addComponent(lblHouse2))
-						.addGap(26)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-								.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-								.addComponent(button_3, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-										.addComponent(button_4, GroupLayout.PREFERRED_SIZE, 37,
-												GroupLayout.PREFERRED_SIZE)
-										.addComponent(button_5, GroupLayout.PREFERRED_SIZE, 37,
-												GroupLayout.PREFERRED_SIZE)))
-						.addPreferredGap(ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
-
-						.addComponent(btnReset)));
-		contentPane.setLayout(gl_contentPane);
+	
 	}
-
-	private class ButtonsListener implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent event) {
-			JButton source = (JButton) event.getSource();
-
+		private class ButtonsListener implements ActionListener {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.print(e.getActionCommand());
+			}
 		}
-	}
+	
 }
