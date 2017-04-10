af8c88e0036b26abf2326c8511ca97de9b1c2368
chaungo <chaungo@tamu.edu>
2017-04-09 21:23:49 -0500

    updated

--- src/AI.java
+++ src/AI.java
@@ -1,93 +1,3 @@
-/*
-	Project: Kalah_315
-	File: AI.java
-	Authors: 
-*/
-
-/*import java.util.Random;
-import java.util.Vector;
-
-public class AI extends Player{
-	private char type;
-	private Vector<Integer> memory;
-	
-	public AI(char c) {
-		super("computer");
-		type = c;
-		memory = new Vector<Integer>();
-	}
-	
-	public AI(AI ai) {
-		super("computer");
-		type = ai.getType();
-		memory = ai.getMemory();
-	}
-	
-	public char getType () {
-		return type;
-	}
-	
-	public void setType (char c) {
-		type = c;
-	}
-	
-	public Vector<Integer> getMemory() {
-		return memory;
-	}
-	
-	public String runWhat(char c) {
-		String move = "0 0";
-		switch(c)
-		{
-			case 'E':
-				move = runE();
-				break;
-			case 'M':
-				move = runM();
-				break;
-			case 'H':
-				move = runH();
-				break;
-			default:
-				break;
-		}
-		return move;
-	}
-	
-	public String runE() {
-		//pick randomly from valid moves
-		//Random rand = new Random();
-		String move = "0 0";
-		//int max = 13;
-		//pick randomly from valid moves
-		//while (!isValidMove(move)) {
-		//	move = rand.nextInt(1, max);
-		//}
-		return move;
-	}
-	
-	public String runM() {
-		String move = "0 0";
-		//use min-max tree to look ahead
-		return move;
-	}
-
-	public String runH() {
-		String move = "0 0";
-		//use min-max tree to look ahead
-		return move;
-	}
-
-
-}
-
-//simple AI to pick randomly from valid moves
-
-//if AI is P1
-//pick random from these values
-//else
-//pick random from other values
-*/
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Vector;
@@ -107,6 +17,7 @@ public class AI {
 			Board newboard = null;
 			if (current_board.north_house.get(i).nseed != 0) {
 				newboard = current_board.AImoveNorth(i);
+				newboard.move_index = i;
 				searchinglist.add(newboard);
 			}
 		}
@@ -121,6 +32,7 @@ public class AI {
 					Board newboard = null;
 					if (b.north_house.get(i).nseed != 0) {
 						newboard = b.AImoveNorth(i);
+						newboard.move_index = i;
 						seclevel_boards.add(newboard);
 						nochild = false;
 					}
@@ -131,6 +43,7 @@ public class AI {
 					Board newboard = null;
 					if (b.south_house.get(i).nseed != 0) {
 						newboard = b.AImoveSouth(i);
+						newboard.move_index = i;
 						seclevel_boards.add(newboard);
 						nochild = false;
 					}
